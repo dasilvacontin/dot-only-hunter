345898b339622fcf4d5e2852ca202b1d00bf96f6
David da Silva <dasilvacontin@gmail.com>
2019-06-14 15:38:50 +0200

    Add failing test that tests for "test" folder existence

--- tests.sh
+++ tests.sh
@@ -17,4 +17,14 @@ if [[ $res -ne 1 ]]; then
   exit 1
 fi
 
+echo "\n - Exit code should be \`1\` when folder (default or chosen) isn't found."
+# dot-only-hunter will try to find the 'test' folder, and since there isn't
+# one, it should fail. To catch people from not configuring the tool properly.
+./bin/dot-only-hunter
+res=$?
+echo "Exit code: $res"
+if [[ $res -ne 1 ]]; then
+  exit 1
+fi
+
 echo "\nAll good! <3"
